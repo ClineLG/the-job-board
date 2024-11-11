93e55710dd5b150085d321ef87aa5fbb9250e520
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2024-11-11 19:52:56 +0100

    change path app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import Jobs from "./components/jobs";
+import Jobs from "./components/Jobs";
 import Footer from "./components/Footer";
 
 function App() {
